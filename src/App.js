000e20bce731890858044f0d70e75f64f1651472
pk <pk@pk.pk>
2018-11-28 13:54:37 +0100

    k00

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,29 +6,15 @@ import Simple from './ejemplos/Simple.jsx'
 import Airport from './ejemplos/Airport.jsx'
 // import Simple from './ejemplos/Simple.jsx'
 
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-
-        </header>
-        <Simple />
-<Airport />
+        <h1>
+          ejemplos de echarts-for-react
+        </h1>
+        <Simple/>
+        <Airport/>
       </div>
     );
   }

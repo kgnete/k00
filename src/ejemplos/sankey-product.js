db3a619eb6aa0939eff0bd8685d4b4c38ea7aa68
pk <pk@pk.pk>
2018-11-29 00:21:08 +0100

    k00

---
+++ src/ejemplos/sankey-product.js
@@ -0,0 +1,34 @@
+myChart.showLoading();
+$.get('data/asset/data/product.json', function (data) {
+    myChart.hideLoading();
+
+    myChart.setOption(option = {
+        title: {
+            text: 'Sankey Diagram'
+        },
+        tooltip: {
+            trigger: 'item',
+            triggerOn: 'mousemove'
+
+        },
+        series: [
+            {
+                type: 'sankey',
+                data: data.nodes,
+                links: data.links,
+                focusNodeAdjacency: true,
+                itemStyle: {
+                    normal: {
+                        borderWidth: 1,
+                        borderColor: '#aaa'
+                    }
+                },
+                lineStyle: {
+                    normal: {
+                        curveness: 0.5
+                    }
+                }
+            }
+        ]
+    });
+});

e5e7ceec892f99ad7a7df619acd990041821e9cb
pk <pk@pk.pk>
2018-11-28 20:35:54 +0100

    k00

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ReactEcharts from "echarts-for-react";
 import BasicLineChart from './ejemplos/BasicLineChart.jsx'
 import Basicareachart from './ejemplos/Basicareachart.jsx'
 import arearainfall from './ejemplos/area-rainfall.jsx'
-
+import dynamicdata2 from './ejemplos/dynamic-data2.jsx'
 class App extends Component {
   render() {
     return (
@@ -13,6 +13,7 @@ class App extends Component {
         <h1>ejemplos de https://ecomfe.github.io/echarts-examples/public/index.html</h1>
         
         
+        <ReactEcharts option={dynamicdata2}/>
         <ReactEcharts option={arearainfall}/>
         <ReactEcharts option={Basicareachart}/>
         <ReactEcharts option={BasicLineChart}/>

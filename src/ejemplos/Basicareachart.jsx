24777fb43f3d6b339df092fdd9f50f5be02c524d
pk <pk@pk.pk>
2018-11-28 20:25:33 +0100

    k00

--- src/ejemplos/Basicareachart.jsx
+++ src/ejemplos/Basicareachart.jsx
@@ -1,18 +1,35 @@
 const option = {
   xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    type: 'category',
+    boundaryGap: false,
+    data: [
+      'Mon',
+      'Tue',
+      'Wed',
+      'Thu',
+      'Fri',
+      'Sat',
+      'Sun'
+    ]
   },
   yAxis: {
-      type: 'value'
+    type: 'value'
   },
-  series: [{
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
+  series: [
+    {
+      data: [
+        820,
+        932,
+        901,
+        934,
+        1290,
+        1330,
+        1320
+      ],
       type: 'line',
       areaStyle: {}
-  }]
+    }
+  ]
 };
-
 
 export default option

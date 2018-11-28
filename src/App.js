a0b1ad953fa75a257ddab5a5cefd125a0257d199
pk <pk@pk.pk>
2018-11-28 19:03:04 +0100

    k00

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactEcharts from "echarts-for-react";
 
-
-
+import option1 from './ejemplos/BasicLineChart.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <p>
-          git checkout b0
+            git checkout b0
           </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Learn React
           </a>
         </header>
+        <ReactEcharts option={option1}/>
       </div>
     );
   }

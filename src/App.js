5f17ba573836783b86e114774280f4cc97b95908
pk <pk@pk.pk>
2018-11-29 01:31:23 +0100

    k00

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React, {Component} from 'react';
 import './App.css';
 import ReactEcharts from "echarts-for-react";
 
+
+
+
+
 import areabasic from './ejemplos/area-basic.js'
 import arearainfall from './ejemplos/area-rainfall.js'
 // import areasimple from './ejemplos/area-simple.js'
 import areastack from './ejemplos/area-stack.js'
 import multipleyaxis from './ejemplos/multiple-y-axis.js'
-import dynamicdata from './ejemplos/dynamic-data.js'
+// import dynamicdata from './ejemplos/dynamic-data.js'
 
 
 class App extends Component {
@@ -17,7 +21,7 @@ class App extends Component {
       <div className="App">
         <h1>ejemplos de https://github.com/ecomfe/echarts-examples/tree/gh-pages/public/data</h1>
         
-        <ReactEcharts option={dynamicdata}/>
+        {/* <ReactEcharts option={dynamicdata}/> */}
         <ReactEcharts option={multipleyaxis}/>
         <ReactEcharts option={areastack}/>
         {/* <ReactEcharts option={areasimple}/> */}

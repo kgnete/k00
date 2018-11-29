fd1d067aa5f2ca1e33155f393c6076cc4d09e150
pk <pk@pk.pk>
2018-11-29 19:17:59 +0100

    k00

--- src/App.js
+++ src/App.js
@@ -1,209 +1,58 @@
 import React, {Component} from 'react';
-import ReactEcharts from 'echarts-for-react';
+import './App.css';
 
-require('echarts/map/js/china.js');
+// import Airport from './ejemplos/Airport.jsx'
+// import Api from './ejemplos/Api.jsx'
+// // import Calendar from './ejemplos/Calendar.jsx'
+// import Dynamic from './ejemplos/Dynamic.jsx'
+// import Events from './ejemplos/Events.jsx'
+// import Gauge from './ejemplos/Gauge.jsx'
+// import Gcalendar from './ejemplos/Gcalendar.jsx'
+// import Gl from './ejemplos/Gl.jsx'
+// import Graph from './ejemplos/Graph.jsx'
+// import Loading from './ejemplos/Loading.jsx'
+// import Lunar from './ejemplos/Lunar.jsx'
+import Map from './ejemplos/Map.jsx'
+// import Map1 from './ejemplos/Map.1.jsx'
+// import Simple from './ejemplos/Simple.jsx'
+// import Sunburst from './ejemplos/Sunburst.jsx'
+// import Svg from './ejemplos/Svg.jsx'
+// import Theme from './ejemplos/Theme.jsx'
+// import Treemap from './ejemplos/Treemap.jsx'
 
-export default class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.state = this.getInitialState();
-  }
-  timeTicket = null;
-  getInitialState = () => ({
-    option: this.getOption()
-  });
 
-  componentDidMount() {
-    if (this.timeTicket) {
-      clearInterval(this.timeTicket);
-    }
-    this.timeTicket = setInterval(() => {
-      const option = this.state.option;
-      const r = new Date().getSeconds();
-      option.title.text = 'iphone销量' + r;
-      option.series[0].name = 'iphone销量' + r;
-      option.legend.data[0] = 'iphone销量' + r;
-      this.setState({option: option});
-    }, 1000);
-  };
-  componentWillUnmount() {
-    if (this.timeTicket) {
-      clearInterval(this.timeTicket);
-    }
-  };
 
-  randomData() {
-    return Math.round(Math.random() * 1000);
-  };
-
-  getOption = () => {
-    return {
-      title: {
-        text: 'probando json',
-        subtext: '纯属虚构',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item'
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left',
-        data: ['iphone3', 'iphone4', 'iphone5']
-      },
-      visualMap: {
-        min: 0,
-        max: 2500,
-        left: 'left',
-        top: 'bottom',
-        text: [
-          '高', '低'
-        ], // 文本，默认为数值文本
-        calculable: true
-      },
-      toolbox: {
-        show: true,
-        orient: 'vertical',
-        left: 'right',
-        top: 'center',
-        feature: {
-          dataView: {
-            readOnly: false
-          },
-          restore: {},
-          saveAsImage: {}
-        }
-      },
-      series: [
-        {
-          name: 'iphone3',
-          type: 'map',
-          mapType: 'china',
-          roam: false,
-          label: {
-            normal: {
-              show: true
-            },
-            emphasis: {
-              show: true
-            }
-          },
-          data: [
-            {
-              name: '北京',
-              value: this.randomData()
-            }, {
-              name: '天津',
-              value: this.randomData()
-            }, {
-              name: '上海',
-              value: this.randomData()
-            }, {
-              name: '重庆',
-              value: this.randomData()
-            },
-            // {name: '河北',value: this.randomData() }, {name: '河南',value: this.randomData()
-            // }, {name: '云南',value: this.randomData() }, {name: '辽宁',value:
-            // this.randomData() }, {name: '黑龙江',value: this.randomData() }, {name:
-            // '湖南',value: this.randomData() }, {name: '安徽',value: this.randomData() },
-            // {name: '山东',value: this.randomData() }, {name: '新疆',value: this.randomData()
-            // }, {name: '江苏',value: this.randomData() }, {name: '浙江',value:
-            // this.randomData() }, {name: '江西',value: this.randomData() }, {name:
-            // '湖北',value: this.randomData() }, {name: '广西',value: this.randomData() },
-            // {name: '甘肃',value: this.randomData() }, {name: '山西',value: this.randomData()
-            // }, {name: '内蒙古',value: this.randomData() }, {name: '陕西',value:
-            // this.randomData() }, {name: '吉林',value: this.randomData() }, {name:
-            // '福建',value: this.randomData() }, {name: '贵州',value: this.randomData() },
-            // {name: '广东',value: this.randomData() }, {name: '青海',value: this.randomData()
-            // }, {name: '西藏',value: this.randomData() }, {name: '四川',value:
-            // this.randomData() }, {name: '宁夏',value: this.randomData() }, {name:
-            // '海南',value: this.randomData() }, {name: '台湾',value: this.randomData() },
-            // {name: '香港',value: this.randomData() }, {name: '澳门',value: this.randomData()
-            // }
-          ]
-        }, {
-          name: 'conectado',
-          type: 'map',
-          mapType: 'china',
-          label: {
-            normal: {
-              show: true
-            },
-            emphasis: {
-              show: true
-            }
-          },
-          data: [
-            // {name: '北京',value: this.randomData() }, {name: '天津',value: this.randomData()
-            // }, {name: '上海',value: this.randomData() }, {name: '重庆',value:
-            // this.randomData() }, {name: '河北',value: this.randomData() }, {name:
-            // '安徽',value: this.randomData() }, {name: '新疆',value: this.randomData() },
-            // {name: '浙江',value: this.randomData() }, {name: '江西',value: this.randomData()
-            // }, {name: '山西',value: this.randomData() }, {name: '内蒙古',value:
-            // this.randomData() }, {name: '吉林',value: this.randomData() }, {name:
-            // '福建',value: this.randomData() }, {name: '广东',value: this.randomData() },
-            // {name: '西藏',value: this.randomData() },
-            {
-              name: '四川',
-              value: this.randomData()
-            }, {
-              name: '宁夏',
-              value: this.randomData()
-            }, {
-              name: '香港',
-              value: this.randomData()
-            }, {
-              name: '澳门',
-              value: this.randomData()
-            }
-          ]
-        }, {
-          name: 'iphone5',
-          type: 'map',
-          mapType: 'china',
-          label: {
-            normal: {
-              show: true
-            },
-            emphasis: {
-              show: true
-            }
-          },
-          data: [
-            {
-              name: '北京',
-              value: this.randomData()
-            },
-            // {name: '天津',value: this.randomData() }, {name: '上海',value: this.randomData()
-            // }, {name: '广东',value: this.randomData() }, {name: '台湾',value:
-            // this.randomData() },
-            {
-              name: '香港',
-              value: this.randomData()
-            }, {
-              name: '澳门',
-              value: this.randomData()
-            }
-          ]
-        }
-      ]
-    };
-  };
-
+class App extends Component {
   render() {
     return (
-      <div className='examples'>
-        <div className='parent'>
+      <div className="App">
+        <h1>
+          ejemplos de echarts-for-react
+        </h1>
+        {/* <Dynamic/>
 
-          <ReactEcharts
-            option={this.state.option}
-            style={{
-            height: '500px',
-            width: '100%'
-          }}
-            className='react_for_echarts'/>
-        </div>
+        <Treemap/>
+        <Theme/>
+        <Svg/>
+        <Sunburst/>
+        <Simple/> */}
+        {/* <Map1/> */}
+        <Map/>
+        {/* <Lunar/>
+        <Loading/>
+        <Graph/>
+        <Gl/>
+        <Gcalendar/>
+        <Gauge/>
+        <Events/> */}
+
+        {/* <Dynamic/> */}
+        {/* <Calendar/> */}
+        {/* <Api/>
+        <Airport/> */}
       </div>
     );
-  };
+  }
 }
+
+export default App;

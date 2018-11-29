75280f856b58ec0c47b10fb6c9ae120bd843c09d
pk <pk@pk.pk>
2018-11-29 03:13:08 +0100

    k00

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>ejemplos de https://ecomfe.github.io/echarts-examples/public/index.html</h1>
+        <h1>ejemplos de https://github.com/jackshawn/pics-by-echarts</h1>
         
         
         < River />

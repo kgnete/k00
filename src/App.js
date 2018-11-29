5690be3941fefb6aab4853af2c18dffb90982b34
pk <pk@pk.pk>
2018-11-29 02:14:48 +0100

    k00

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import ReactEcharts from "echarts-for-react";
-import BasicLineChart from './ejemplos/BasicLineChart.jsx'
-import Basicareachart from './ejemplos/Basicareachart.jsx'
-import arearainfall from './ejemplos/area-rainfall.jsx'
+
 // import dynamicdata2 from './ejemplos/dynamic-data2.jsx'
 
 class App extends Component {
@@ -14,10 +12,7 @@ class App extends Component {
         <h1>ejemplos de https://ecomfe.github.io/echarts-examples/public/index.html</h1>
         
         
-        {/* <ReactEcharts option={dynamicdata2}/> */}
-        <ReactEcharts option={arearainfall}/>
-        <ReactEcharts option={Basicareachart}/>
-        <ReactEcharts option={BasicLineChart}/>
+
       </div>
     );
   }

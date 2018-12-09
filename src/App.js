4c100e4373020116dbcede037b9270224363ec27
pk <pk@pk.pk>
2018-12-09 01:59:20 +0100

    k00

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
 
-const AppRouter = () => (
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about/">About</Link>
-          </li>
-          <li>
-            <Link to="/users/">Users</Link>
-          </li>
-        </ul>
-      </nav>
 
-      <Route path="/" exact component={Index} />
-      <Route path="/about/" component={About} />
-      <Route path="/users/" component={Users} />
-    </div>
-  </Router>
-);
 
-export default AppRouter;
+
+
+
+
+
+
+
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>jojoj</h1>
+      </div>
+    )
+  }
+}
+
+
